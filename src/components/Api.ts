c919afb60c42596d95c8886408ea8c1d32342aac
João Victor <52728391+JoaoVic7or@users.noreply.github.com>
2023-05-30 15:43:47 -0300

    update

--- src/components/Api.ts
+++ src/components/Api.ts
@@ -1,7 +1,18 @@
-export async function fetchData (cidade:string) {
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=3c113e590e40568e4674ee6990b9fe3a&lang=pt_br&units=metric`
-      );
-      const data = await response.json();
+export async function fetchData(cidade: string) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=3c113e590e40568e4674ee6990b9fe3a&lang=pt_br&units=metric`
+  );
+  try {
+    const data = await response.json();
+    console.log(data)
+    if(data.cod == '404'){
+      alert('Cidade não encontrada!')
+    }
+    else{
       return data;
+    }
+  }
+  catch (error){
+    console.log(error)
+  }
 }
